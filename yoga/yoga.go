e74f848bb363cb53ba63037b63390006534aa7f5
Henrik Jonsson <hekjon@gmail.com>
2013-12-07 11:36:12 -0800

    Simple login

--- yoga/yoga.go
+++ yoga/yoga.go
@@ -3,10 +3,14 @@ package yoga
 import (
     "fmt"
     "net/http"
+
+		"appengine"
+		"appengine/user"
 )
 
 func init() {
-    http.HandleFunc("/", handler)
+    http.HandleFunc("/", indexHandler)
+    http.HandleFunc("/login", loginHandler)
 }
 
 const embedPage = `
@@ -41,6 +45,22 @@ div#main {
 </html>
 `
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, embedPage)
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+		 fmt.Fprint(w, embedPage)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+		 c := appengine.NewContext(r)
+		 u := user.Current(c)
+		 if u == nil {
+		 		url, err := user.LoginURL(c, r.URL.String())
+				if err != nil {
+					 http.Error(w, err.Error(), http.StatusInternalServerError)
+					 return
+				}
+				w.Header().Set("Location", url)
+				w.WriteHeader(http.StatusFound)
+				return
+		 }
+		 fmt.Fprintf(w, "Hello, %v!", u)
 }
